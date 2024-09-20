64e3121731899b36bcc5034c395c63a0adcdfa10
Shuva Dev <s.r.shuvadeb@gmail.com>
2024-09-20 18:16:18 +0600

    Add project files.

---
+++ DataImportExportToolkit/Controllers/ExportToolkit.cs
@@ -0,0 +1,20 @@
+﻿using DataImportExportToolkit.Models;
+using Microsoft.AspNetCore.Mvc;
+using Rotativa.AspNetCore;
+
+namespace DataImportExportToolkit.Controllers
+{
+    public class ExportToolkit : Controller
+    {
+        public IActionResult GeneratePDF()
+        {
+            Person person = new Person() { Name = "Shuva Dev", Age = 22 };
+
+            return new ViewAsPdf("GeneratePDF", person, ViewData)
+            {
+                PageMargins = new Rotativa.AspNetCore.Options.Margins() { Top = 20, Bottom = 20, Left = 20, Right = 20 },
+                PageOrientation = Rotativa.AspNetCore.Options.Orientation.Portrait
+            };
+        }
+    }
+}
