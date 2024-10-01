22e6e4c7d516ff8184218957ea3915a8706722bb
Shuva Dev <s.r.shuvadeb@gmail.com>
2024-10-01 11:34:06 +0600

    CSV Configuration completed

--- DataImportExportToolkit/Controllers/ExportToolkit.cs
+++ DataImportExportToolkit/Controllers/ExportToolkit.cs
@@ -1,6 +1,8 @@
-﻿using DataImportExportToolkit.Models;
+﻿using CsvHelper;
+using DataImportExportToolkit.Models;
 using Microsoft.AspNetCore.Mvc;
 using Rotativa.AspNetCore;
+using System.Globalization;
 
 namespace DataImportExportToolkit.Controllers
 {
@@ -16,5 +18,33 @@ namespace DataImportExportToolkit.Controllers
                 PageOrientation = Rotativa.AspNetCore.Options.Orientation.Portrait
             };
         }
+
+        public async Task<IActionResult> GenerateCSV()
+        {
+            List<Person> persons = new()
+            {
+                new() {Name = "Shuva", Age = 23},
+                new() {Name = "Shanto", Age = 20}
+            };
+
+
+            MemoryStream memoryStream = new MemoryStream();
+            StreamWriter streamWriter = new StreamWriter(memoryStream);
+
+            CsvWriter csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture, leaveOpen: true);
+
+            csvWriter.WriteHeader<Person>();
+            csvWriter.NextRecord();
+
+            await csvWriter.WriteRecordsAsync(persons);
+            //csvWriter.WriteField(persons[0].Name);
+            await streamWriter.FlushAsync();
+
+            memoryStream.Position = 0;
+
+            return File(memoryStream, "text/csv", "test.csv");
+        }
+
+
     }
 }
